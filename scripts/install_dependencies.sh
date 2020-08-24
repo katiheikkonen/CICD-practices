e3b13bcdefbf328202a867cd629a46c44fce6697
katiheikkonen <kati.m.heikkonen@gmail.com>
2020-08-24 15:28:33 +0300

    added artifacts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,3 @@
-        python -m pip install --upgrade pip
-        pip install flake8 pytest
-        if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+python -m pip install --upgrade pip
+pip install flake8 pytest
+if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
